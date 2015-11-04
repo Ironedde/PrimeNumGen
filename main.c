2c3d2b735cc5de92d34494a9ac75b17cb504a04d
Edvin Åkerfeldt <ironedde@taurus.ludd.ltu.se>
2015-11-04 17:13:21 +0100

    Started to add threading

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
+//Included for threading
+#include <pthread.h>
 
-
-
-
-
-int primes[8096];
+int curr_arr_size = 1024*1024;
+int* primes;
 int ind = 2;						//Array start
 
+
+//Threads
+
+
+//Sum of the digits in the number
 int digit_sum(int n) {
 
   int r = 0;
@@ -35,12 +40,22 @@ int is_prime(int prime) {
 
 
 int main(int argc, char** argv) {
+ 
+  primes = malloc(curr_arr_size);
   primes[0] = 5;					//
   primes[1] = 7;					//Basic primes
-
+  //The way to create threads
+  //int pthread_create(pthread_t *restrict tidp, const pthread_attr_t *restrict attr, void *(*start_rtn)(void), void *restrict arg)
+  //int pthread_create(pthread_t *restrict tidp, NULL, void *(*start_rtn)(void), NULL);
   int i;
+  char ch; 
   for(i = 9 ;; i += 2) {				//Every 2:nd
     if(is_prime(i)) {					//If prime
+      if(ind+1 == curr_arr_size){
+	   printf("Increasing array size");
+   	   scanf("%c", &ch);
+      	   primes = realloc(primes,curr_arr_size*2);    
+      }
       primes[ind++] = i;				//Insert into array
       printf("Found prime: %d\n", i);			
     }
